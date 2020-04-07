089f7a576374fbb060310b37a39a3f644ea9c1de
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-04-07 09:57:21 -0700

    Added test codes

--- Lab_02/Lab_02/src/main.c
+++ Lab_02/Lab_02/src/main.c
@@ -33,6 +33,7 @@
  * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
  */
 #include <asf.h>
+#include "header/io.h"
 
 int main (void)
 {
@@ -41,4 +42,11 @@ int main (void)
 	board_init();
 
 	/* Insert application code here, after the board has been initialized. */
+	printf("Hello World\n");
+	DDRB = 0xFF; // Configure port B's 8 pins as outputs
+	PORTB = 0x00; // Initialize PORTB output to 0¡¯s
+	while(1){
+		PORTB = 0x0F; // Writes port B's 8 pins with 00001111
+	}
+	return 1;
 }
