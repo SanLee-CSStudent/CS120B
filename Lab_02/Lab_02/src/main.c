86a8c21506d55b0ef57de2df827aa34f8a6e2fb3
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-04-07 09:40:20 -0700

    Initial commit

---
+++ Lab_02/Lab_02/src/main.c
@@ -0,0 +1,44 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+	Project name: Lab2_introToAVR
+	Partners name [none]: Jo Smith
+	Microcontroller [atmega1284]:
+	 Clock Frequency [8000000]:
+ */
+
+/**
+ * \mainpage User Application template doxygen documentation
+ *
+ * \par Empty user application template
+ *
+ * Bare minimum empty user application template
+ *
+ * \par Content
+ *
+ * -# Include the ASF header files (through asf.h)
+ * -# "Insert system clock initialization code here" comment
+ * -# Minimal main function that starts with a call to board_init()
+ * -# "Insert application code here" comment
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+#include <asf.h>
+
+int main (void)
+{
+	/* Insert system clock initialization code here (sysclk_init()). */
+
+	board_init();
+
+	/* Insert application code here, after the board has been initialized. */
+}
