8b15c73d0f2cbdce4d9973e256cafb415056ce27
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-04-09 10:47:36 -0700

    Added commands.txt

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -14,12 +14,12 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs --> PINA
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
     /* Insert your solution below */
     while (1) {
 		
-		if((PORTA & 0x03) == 0x01){
+		if((PINA & 0x03) == 0x01){
 			PORTB = 0x01;
 		}
 		else{
