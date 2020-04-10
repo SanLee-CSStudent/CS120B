399f8a097e402a7c8bb82b62284f73d6483be098
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-04-10 13:56:48 -0700

    Update main.c

--- Lab3_bitManipulation/source/main.c
+++ Lab3_bitManipulation/source/main.c
@@ -25,23 +25,20 @@ int main(void) {
 		if(level <= 2){
 			PORTC = PORTC | 0x60;
 		}
-		else if(level > 2 && level <= 4){
+		if(level > 2 && level <= 4){
 			PORTC = PORTC | 0x70;
 		}
-		else if(level > 4 && level <= 6){
+		if(level > 4 && level <= 6){
 			PORTC = PORTC | 0x38;	
 		}
-		else if(level > 6 && level <= 9){
+		if(level > 6 && level <= 9){
 			PORTC = PORTC | 0x3C;	
 		}
-		else if(level > 9 && level <= 12){
+		if(level > 9 && level <= 12){
 			PORTC = PORTC | 0x3E;
 		}
-		else if(level > 12 && level <= 15){
+		if(level > 12 && level <= 15){
 			PORTC = PORTC | 0x3F;
-		}
-		else{
-			PORTC = PORTC | 0x60;
 		}
 
     }
