fab2d0faff1f5dd5619fe69fa5e5a559bdd5a25a
San Lee <slee488@wch136-27.cs.ucr.edu>
2020-04-10 07:33:07 -0700

    Copied into turnin

---
+++ Lab2_introToAVR/turnin/slee488_lab2_part1.c
@@ -0,0 +1,33 @@
+/*	Author: slee488
+ *  Partner(s) Name: NA
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0x00; // Configure port A's 8 pins as inputs --> PINA
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+    unsigned char tempA;
+	/* Insert your solution below */
+    while (1) {
+
+		tempA = PINA & 0x03;
+	
+		if((tempA) == 0x01){
+			PORTB = 0x01;
+		}
+		else{
+			PORTB = 0x00;
+		}
+    }
+    return 1;
+}
