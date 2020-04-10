b42568527c6f829ba5da8e1745261a35c0cf1368
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-04-10 08:20:13 -0700

    Update main.c

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -17,22 +17,24 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0x00; // Configure port A's 8 pins as inputs --> PINA
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
-	unsigned char temp;
+	unsigned char tempA;
+	unsigned char tempC;
     unsigned char cntavail;
 	unsigned char i;
 	/* Insert your solution below */
     while (1) {
 		cntavail = 0x00;
-		temp = PINA & 0x0F;
+		tempA = PINA & 0x0F;
 		
-		/*for(i = 3; i >= 0; i--){
-			if(((temp >> i) & 0x01) == 0x01){
+		for(i = 3; i >= 0; i--){
+			tempC = (tempA >> i) & 0x01;
+			if(tempC == 0x01){
 				cntavail = cntavail;
 			}
 			else{
 				cntavail = cntavail + 1;
 			}
-		}*/
+		}
 		
 		PORTC = cntavail;
 		
