6388667ff3f31281bc9fbad6831080c0e3f63c09
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-04-23 18:44:07 -0700

    Added Lab5

---
+++ Lab5_Atmega1284/source/main.c
@@ -0,0 +1,47 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    /* Insert your solution below */
+    unsigned char level;
+    while (1) {
+	level = PINA & 0x0F;
+	if(level == 0x00){
+	    PORTC = 0x40;
+	}
+	if(level > 0x00 && level <= 0x02){
+            PORTC = 0x60;
+        }
+	if(level > 0x02 && level <= 0x04){
+	    PORTC = 0x70;
+	}
+	if(level > 0x04 && level <= 0x06){
+            PORTC = 0x38;
+        }
+	if(level > 0x06 && level <= 0x09){
+            PORTC = 0x3C;
+        }
+	if(level > 0x09 && level <= 0x0C){
+            PORTC = 0x3E;
+        }
+	if(level > 0x0C && level <= 0x0F){
+            PORTC = 0x3F;
+        }
+
+    }
+    return 1;
+}
