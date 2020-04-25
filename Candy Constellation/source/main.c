0e386c0864b2f58aa60701cca9994f783f3a46d9
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-04-25 15:40:06 -0700

    Update main.c

--- Candy Constellation/source/main.c
+++ Candy Constellation/source/main.c
@@ -12,12 +12,63 @@
 #include "simAVRHeader.h"
 #endif
 
+const double[8] keyFrequencies{261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
+unsigned char i;
+void set_PWM(double frequency){
+    static double current_frequency;
+
+    if(frequency != current_frequency){
+        if(!frequency){
+            TCCR3B &= 0x08;
+        }
+        else{
+            TCCRB |= 0x03;
+        }
+
+        if(frequency < 0.954){
+            OCR3A = 0xFFFF;
+        }
+        else if(frequency > 31250){
+            OCR3A = 0x0000;
+        }
+        else{
+            OCR3A = (short) (8000000 / (128 * frequency)) - 1;
+        }
+
+        TCNT3 = 0;
+        current_frequency = frequency;
+    }
+}
+
+void PWM_on(){
+    TCCR3A = (1 << COM3A0);
+
+    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+
+    set_PWM(0);
+}
+
+void PWM_off(){
+    TCCR3A = 0x00;
+    TCCR3B = 0x00;
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
     /* Insert your solution below */
+    PWM_on();
     while (1) {
 
+        if(PINA == 0x01){
+            i++;
+            set_PWM(keyFrequencies[i%8]);
+        }
+        if(PINA == 0x02){
+            break;
+        }
     }
+    PWM_off();
     return 1;
 }
