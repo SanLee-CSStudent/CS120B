fcdcb6d1645487d5cc829863f71550dd9ec19f89
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-05-03 10:45:28 -0700

    Update main.c

--- Lab6_SynchSMs/source/main.c
+++ Lab6_SynchSMs/source/main.c
@@ -13,19 +13,69 @@
 #include "timer.h"
 #endif
 
+enum STATE{Start, ONE, TWO, THREE} states;
+unsigned LEDC = 0x00;
 
+void Tick(){
+
+    switch(states){
+        case Start:
+            states = ONE;
+            break;
+
+        case ONE:
+            states = TWO;
+            break;
+
+        case TWO:
+            states = THREE;
+            break;
+
+        case THREE:
+            states = ONE;
+            break;
+        
+        default:
+            states = ONE;
+            break;
+    }
+
+    switch(states){
+        case Start:
+            break;
+
+        case ONE:
+            LEDC = 0x01;
+            break;
+
+        case TWO:
+            LEDC = 0x02;
+            break;
+
+        case THREE:
+            LEDC = 0x04;
+            break;
+        
+        default:
+            states = ONE;
+            break;
+    }
+
+}
 
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRC = 0xFF; PORTC = 0x00;
+
+    states = Start;
+
     TimerSet(1000);
     TimerOn();
-    unsigned char tmpC = 0x00;
 
     /* Insert your solution below */
     while (1) {
-        tmpC = ~tmpC;
-        PORTC = tmpC;
+        Tick();
+        PORTC = LEDC;
         while(!TimerFlag){}
         TimerFlag = 0;
     }
