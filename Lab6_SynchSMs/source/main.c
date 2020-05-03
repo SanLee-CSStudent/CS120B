ec7c43798505d69f23e32a84656e313a6937c464
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-05-03 10:13:02 -0700

    Update main.c

--- Lab6_SynchSMs/source/main.c
+++ Lab6_SynchSMs/source/main.c
@@ -17,15 +17,15 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xFF; PORTC = 0x00;
     TimerSet(1000);
     TimerOn();
-    unsigned char tmpB = 0x00;
+    unsigned char tmpC = 0x00;
 
     /* Insert your solution below */
     while (1) {
-        tmpB = ~tmpB;
-        PORTB = tmpB;
+        tmpC = ~tmpC;
+        PORTC = tmpB;
         while(!TimerFlag){}
         TimerFlag = 0;
     }
