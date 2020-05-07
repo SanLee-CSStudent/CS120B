5c596d6b29bb174159f41b980e97f8035a261835
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-05-07 11:11:29 -0700

    Added ex1

--- Lab7_LCDScreen/source/main.c
+++ Lab7_LCDScreen/source/main.c
@@ -13,18 +13,141 @@
 #include "io.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRC = 0xFF; PORTC = 0x00;
-    DDRD = 0xFF; PORTD = 0x00;
+enum STATE{Start, Init, Inc, Dec, IncR, DecR, IncP, DecP, ResetR, Reset} state;
+unsigned char button = 0x00;
+unsigned char count = 0x00;
 
+void Tick(){
+	
+	switch(state){
+		case Start:
+			state = Init;
+			break;
+		case Init:
+			if(button == 0x01){
+				state = Inc;
+			}
+			else if(button == 0x02){
+				state = Dec;
+			}
+			else if(button == 0x03){
+				state = Reset;
+			}
+			else {
+				state = Init;
+			}
+
+			break;
+		case Inc:
+			// unsigned char tempA = (PINA & 0x01);
+            if(button == 0x01){
+                state = Inc;
+                // pressing = 0x01;
+            }
+            else if(button == 0x03){
+                state = Reset;
+            }
+            else{
+			    state = IncR;
+            }
+			
+			break;
+		
+		case IncR:
+			state = Init;
+			
+			break;
+
+		case Dec:
+			// unsigned char tempB = (PINA & 0x02);
+			if(button == 0x02){
+				state = Dec;
+			}
+            if(button == 0x03){
+                state = Reset;
+            }
+			else{
+				state = DecR;
+			}
+			break;
+
+		case DecR:
+			state = Init;
+			
+			break;
+
+        case Reset:
+            if(button == 0x03){
+                state = Reset;
+            }
+            else{
+                state = ResetR;
+            }
+            break;
+
+		case ResetR:
+			state = Init;
+			break;
+			
+		default:
+			break;
+	}
+	
+	switch(state){
+		case Start:
+			count = 0x00;
+		break;
+		case Init:
+
+		break;
+
+		case Inc:
+	
+			
+			break;
+
+		case IncR:
+			if(PORTC < 9){
+				count++;
+			}
+			LCD_WriteData(count + '0');
+			break;
+		
+		case Dec:
+
+			
+			break;
+
+		case DecR:
+			if(PORTC > 0){
+				count--;
+			}
+			LCD_WriteData(count + '0');
+			break;
+
+		case ResetR:
+			count = 0x00;
+            LCD_WriteData(count + '0');
+		break;
+		default:
+			
+		break;
+	}
+
+    
+}
+
+int main(void) {
+	DDRA = 0x00;PORTA = 0xFF;
+	DDRC = 0xFF;PORTC = 0x00;
+    /* Insert DDR and PORT initializations */
+	state = Start;
     /* Insert your solution below */
     LCD_init();
 
-    LCD_DisplayString(1, "Hello World");
-
     while (1) {
-        continue;
+		button = ~PINA & 0x03;
+		Tick();
     }
     return 1;
 }
