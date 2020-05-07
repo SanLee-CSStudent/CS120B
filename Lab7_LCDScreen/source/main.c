dbbbd35e02091ac272cdeefed8c67d664aa236be
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-05-07 11:18:48 -0700

    Update main.c

--- Lab7_LCDScreen/source/main.c
+++ Lab7_LCDScreen/source/main.c
@@ -13,8 +13,71 @@
 #include "io.h"
 #endif
 
+#include <avr/interrupt.h>
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1ms
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B 	= 0x0B;	// bit3 = 1: CTC mode (clear timer on compare)
+					// bit2bit1bit0=011: prescaler /64
+					// 00001011: 0x0B
+					// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+					// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A 	= 125;	// Timer interrupt will be generated when TCNT1==OCR1A
+					// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+					// So when TCNT1 register equals 125,
+					// 1 ms has passed. Thus, we compare to 125.
+					// AVR timer interrupt mask register
+
+	TIMSK1 	= 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1 = 0;
+
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+	_avr_timer_cntcurr = _avr_timer_M;
+
+	//Enable global interrupts
+	SREG |= 0x80;	// 0x80: 1000000
+}
+
+void TimerOff() {
+	TCCR1B 	= 0x00; // bit3bit2bit1bit0=0000: timer off
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect)
+{
+	// CPU automatically calls when TCNT0 == OCR0 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; 			// Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { 	// results in a more efficient compare
+		TimerISR(); 				// Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+
 enum STATE{Start, Init, Inc, Dec, IncR, DecR, IncP, DecP, ResetR, Reset} state;
 unsigned char button = 0x00;
+unsigned char i = 0x00;
+unsigned char pressing = 0x00;
 unsigned char count = 0x00;
 
 void Tick(){
@@ -41,7 +104,7 @@ void Tick(){
 		case Inc:
 			// unsigned char tempA = (PINA & 0x01);
             if(button == 0x01){
-                state = Inc;
+                state = IncP;
                 // pressing = 0x01;
             }
             else if(button == 0x03){
@@ -52,6 +115,16 @@ void Tick(){
             }
 			
 			break;
+
+		case IncP:
+			if(button == 0x01){
+                pressing = 0x01;
+				state = IncP;
+			}
+			else{
+				state = Init;
+			}
+			break;
 		
 		case IncR:
 			state = Init;
@@ -61,13 +134,22 @@ void Tick(){
 		case Dec:
 			// unsigned char tempB = (PINA & 0x02);
 			if(button == 0x02){
-				state = Dec;
+				state = DecP;
 			}
-            if(button == 0x03){
+            else if(button == 0x03){
                 state = Reset;
             }
 			else{
 				state = DecR;
+			}
+			break;
+
+		case DecP:
+			if(button == 0x02){
+				state = DecP;
+			}
+			else{
+				state = Init;
 			}
 			break;
 
@@ -103,26 +185,53 @@ void Tick(){
 
 		case Inc:
 	
-			
-			break;
-
-		case IncR:
-			if(PORTC < 9){
+			if(count < 9){
 				count++;
 			}
 			LCD_WriteData(count + '0');
 			break;
+
+		case IncP:
+            i += 1;
+            if(i == 10){
+                i = 0;
+                if(count < 9){
+                    count++;
+                }
+                
+                LCD_WriteData(count + '0');
+            }
+
+		break;
+
+		case IncR:
+			
+			break;
 		
 		case Dec:
 
-			
-			break;
-
-		case DecR:
-			if(PORTC > 0){
+			if(count > 0){
 				count--;
 			}
-			LCD_WriteData(count + '0');
+			
+            LCD_WriteData(count + '0');
+			break;
+
+		case DecP:
+            
+            if(i == 10){
+                i = 0;
+                if(count > 0){
+                    count--;
+                }
+                LCD_WriteData(count + '0');
+            }
+			i += 1;
+        
+		break;
+
+		case DecR:
+			
 			break;
 
 		case ResetR:
@@ -133,21 +242,25 @@ void Tick(){
 			
 		break;
 	}
-
-    
 }
 
 int main(void) {
 	DDRA = 0x00;PORTA = 0xFF;
 	DDRC = 0xFF;PORTC = 0x00;
+    DDRD = 0xFF;PORTD = 0x00;
+    
     /* Insert DDR and PORT initializations */
 	state = Start;
     /* Insert your solution below */
     LCD_init();
+    TimerSet(100);
+    TimerOn();
 
     while (1) {
 		button = ~PINA & 0x03;
 		Tick();
+        while(!TimerFlag){}
+        TimerFlag = 0;
     }
     return 1;
 }
