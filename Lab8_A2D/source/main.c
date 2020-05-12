800b32ab3bcef1e9f575b3b656d9318422b7f171
San Lee <56095711+SanLee-CSStudent@users.noreply.github.com>
2020-05-12 11:15:28 -0700

    Update main.c

--- Lab8_A2D/source/main.c
+++ Lab8_A2D/source/main.c
@@ -16,6 +16,7 @@ unsigned short input;
 unsigned short max = 0x00C3;
 unsigned char upper;
 unsigned char lower;
+unsigned char LED;
 
 void ADC_init(){    
     ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
@@ -31,18 +32,32 @@ int main(void) {
     ADC_init();
     while (1) {
         input = ADC;
-        if(input >= (max/2)){
-            lower = (char) input;
-            upper = (char) (input >> 8);
-
-            PORTC = lower;
-            PORTD = upper;
+        if(input < (max/8)){
+            LED = 0x00;
+        }
+        else if(input < (max/4)){
+            LED = 0x01;
+        }
+        else if(input < (3*max / 8)){
+            LED = 0x03;
+        }
+        else if(input < (max / 2)){
+            LED = 0x07;
+        }
+        else if(input < (5*max / 8)){
+            LED = 0x0F;
+        }
+        else if(input < (3*max / 4)){
+            LED = 0x1F;
+        }
+        else if(input < (7*max/8)){
+            LED = 0x3F;
         }
         else{
-            PORTC = 0x00;
-            PORTD = 0x00;
+            LED = 0x7F;
         }
         
+        PORTC = LED;
     }
     return 1;
 }
